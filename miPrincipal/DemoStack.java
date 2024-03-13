810b33b24b7d07dafaa8a68b22fe2765bb1d0772
Jose Alonso Cibrian Robles <125108989+CibrianA04@users.noreply.github.com>
2024-03-13 15:47:50 +0000

    actualización demostack

---
+++ miPrincipal/DemoStack.java
@@ -0,0 +1,22 @@
+package miPrincipal;
+import java.util.Stack;
+public class DemoStack {
+    public static void main(String[] args) {
+        //crear una nueva pila de enteros
+        Stack<Integer> pila = new Stack<Integer>();
+        //apilar elementos con .push()
+        pila.push(2);
+        pila.push(6);
+        pila.push(7);
+        //buscamos elemento
+        System.out.println("El elemento 6 esta en la  posición : " + pila.search(6));
+
+        //mostrar el tope de la pila con .peek()
+        System.out.println("El tope de la pila es: " + pila.peek());
+        //desapilar un elemento con .pop()
+        pila.pop();
+        System.out.println("El tope de la pila es: " + pila.peek());
+        pila.pop();
+        System.out.println("El tope de la pila es: " + pila.peek());
+    }
+}
