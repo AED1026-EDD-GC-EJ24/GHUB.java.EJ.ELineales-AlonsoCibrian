cd8949c2deac2fe7cf3b0cf945b1813f3fc13a0f
Jose Alonso Cibrian Robles <125108989+CibrianA04@users.noreply.github.com>
2024-03-20 15:55:22 +0000

    Actualizacion vuelos

---
+++ miPrincipal/SimulaVuelos.java
@@ -0,0 +1,8 @@
+package miPrincipal;
+
+public class SimulaVuelos {
+    public void Simular(){
+        //crear cola de vuelos
+        Cola<Vuelo> colaVuelos = new Cola<Vuelo>();
+    }
+}
---
+++ miPrincipal/Vuelo.java
@@ -0,0 +1,11 @@
+package miPrincipal;
+
+public class Vuelo {
+    public void leerVueloDesdeArchivo(){
+
+    }
+    public boolean hayAterrizajes(){
+        return true;
+    }
+    
+}
