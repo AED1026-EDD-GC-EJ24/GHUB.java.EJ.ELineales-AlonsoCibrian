f74fab4fae338d47f4f0b2029a6452bd8fcfeb36
Jose Alonso Cibrian Robles <125108989+CibrianA04@users.noreply.github.com>
2024-04-09 15:56:45 +0000

    actualizacion estructura combinada

--- miPrincipal/DemoEstructuraCombinada.java
+++ miPrincipal/DemoEstructuraCombinada.java
@@ -1,5 +1,5 @@
 package miPrincipal;
-import java.util.*;
+import java.util.*; //clase linkedlist, collection
 
 public class DemoEstructuraCombinada {
     public static void menu(){
@@ -8,10 +8,28 @@ public class DemoEstructuraCombinada {
         System.out.println("    HashTable - LinkedList       ");
         System.out.println("=================================");
         System.out.println();
-
-
-
-
+        HashTable2<Integer> ht = new HashTable2<Integer>();
+        ht.put("par", 2);
+        ht.put("par", 4);
+        ht.put("par", 6);
+        ht.put("impar", 1);
+        ht.put("par", 8);
+        ht.put("impar", 3);
+        ht.put("impar", 5);
+        ht.put("impar", 7);
+        ht.put("par", 10);
+        ht.put("impar", 9);
+        LinkedList<Integer> pares = ht.get("par");
+        for(Integer i : pares){
+            System.out.println("Par: " + i);
+        }
+        LinkedList<Integer> impares = ht.get("impar");
+        for(Integer i : impares){
+            System.out.println("Impar: " + i);
+        }
+        Collection<String> claves = ht.keys();
+        for(String k : claves){
+            System.out.println("Clave: " +  k);
+        }
     }
-
 }
--- miPrincipal/HashTable2.java
+++ miPrincipal/HashTable2.java
@@ -1,16 +1,33 @@
 package miPrincipal;
 
-import java.util.*;
+import java.util.*; //clase linkedlist, hastable, vector, collection 
 
 public class HashTable2<T> {
     private Hashtable<String, LinkedList<T>> tabla;
     private Vector<String> claves;
+
     //constructor
     public HashTable2() {
         tabla = new Hashtable<String, LinkedList<T>>();
         claves = new Vector<String>();
     }
-    public void put(String key, T elm){
 
+    public void put(String key, T elm){
+        LinkedList <T> lst = tabla.get(key);
+        if(lst == null){
+            lst = new LinkedList<T>();
+            tabla.put(key, lst);
+            claves.add(key);
+        }
+        lst.addLast(elm);
     }
+
+    //metodo get
+    public LinkedList<T> get(String key){
+        return tabla.get(key);
+    }
+    public Collection <String> keys(){
+        return claves;
+    }
+    
 }
