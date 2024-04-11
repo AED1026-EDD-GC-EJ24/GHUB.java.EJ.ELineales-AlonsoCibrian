0ad8a811aba2098e3279c23eb88c9e0fb914d237
Jose Alonso Cibrian Robles <125108989+CibrianA04@users.noreply.github.com>
2024-04-11 15:52:26 +0000

    cambios de la lista

--- miPrincipal/Lista.java
+++ miPrincipal/Lista.java
@@ -1,3 +1,4 @@
+package miPrincipal;
 public class Lista<T> {
     //primer elemento lista
     private Nodo<T> cabeza;
@@ -32,4 +33,77 @@ public class Lista<T> {
         }
         tamanio++;
     } 
+    //insertar nuevo elemento en la lista
+    public void insertar(T valor, int pos) throws PosicionIlegalException{
+        if (pos >= 0 && pos <= tamanio) {
+            Nodo<T> nuevo = new Nodo<T>();
+            nuevo.setValor(valor);
+            if (pos == 0) {
+                nuevo.setSiguiente(cabeza);
+                cabeza = nuevo;
+            } else{
+               //si el nodo se inserta al final de la lista
+               if (pos == tamanio) {
+                   Nodo<T> aux = cabeza;
+                   while(aux.getSiguiente() != null){
+                       aux = aux.getSiguiente();
+                   }
+                   aux.setSiguiente(nuevo);
+               } 
+               //el nodo se inserta en medio de la lista
+               else{
+                   Nodo<T> aux = cabeza;
+                   for (int i = 0; i <= pos-2; i++) {
+                       aux = aux.getSiguiente();
+                   }
+                   nuevo.setSiguiente(aux.getSiguiente());
+                   aux.setSiguiente(nuevo);
+               }
+            }
+            tamanio++;
+        }
+        else{
+            throw new PosicionIlegalException();
+        }   
+
+    } 
+    public T getValor(int pos) throws PosicionIlegalException{
+        if (pos >= 0 && pos < tamanio) {
+          T valor;
+          if (pos==0) {
+            valor = cabeza.getValor();
+            return valor;
+          }else{
+            Nodo<T> aux = cabeza;
+            for (int i = 0; i <= pos-1; i++) {
+                aux = aux.getSiguiente();
+            }
+            valor = aux.getValor();
+            return valor;
+          }
+        }
+        else{
+            throw new PosicionIlegalException();
+        }
+    }
+    public void remover(int pos) throws PosicionIlegalException{
+        if (pos >= 0 && pos < tamanio) {
+            if (pos == 0) {
+                //eliminar el primer nodo de la lista
+                cabeza = cabeza.getSiguiente();
+                tamanio--;
+            } else{ // remover el ultimo o en medio
+                Nodo<T> aux = cabeza;
+                for (int i = 0; i <= pos-2; i++) {
+                    aux = aux.getSiguiente();
+                }
+                Nodo<T> prox = aux.getSiguiente();
+                aux.setSiguiente(prox.getSiguiente());
+                tamanio--;
+            }
+        }
+        else{
+            throw new PosicionIlegalException();
+        }
+    }
 }
