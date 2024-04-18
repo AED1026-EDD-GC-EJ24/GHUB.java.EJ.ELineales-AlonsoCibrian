0b48644ded66a5c4d7915976a4d722fab8cfdc86
Jose Alonso Cibrian Robles <125108989+CibrianA04@users.noreply.github.com>
2024-04-18 15:54:42 +0000

    ejercicio de Contactos

--- miPrincipal/DemoLinkedListCuadrupedos.java
+++ miPrincipal/DemoLinkedListCuadrupedos.java
@@ -42,6 +42,7 @@ public class DemoLinkedListCuadrupedos {
                     case "Elefante":
                         elefanteCount++;
                         break;
+                    
                 }
             }
 
