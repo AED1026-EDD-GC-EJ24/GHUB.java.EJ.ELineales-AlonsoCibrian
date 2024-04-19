d31904f2a413df8e30ef18c1c7cd170b8293b50a
Jose Alonso Cibrian Robles <125108989+CibrianA04@users.noreply.github.com>
2024-04-19 15:55:32 +0000

    cambios lista contacto

--- EjercicioContactos/DemoListaContactos.java
+++ EjercicioContactos/DemoListaContactos.java
@@ -1,13 +1,36 @@
 package EjercicioContactos;
-
 import miPrincipal.*;
 
 public class DemoListaContactos {
-    public static void menu(){
+    public static void menu() throws PosicionIlegalException{
         System.out.println("**************************");
         System.out.println("Lista de Contactos");
         System.out.println("**************************");
-
-        Lista<Contacto> contactos = new Lista<Contacto>();
-      
+        System.out.println();
+        ListaDeContactos ldc = new ListaDeContactos();
+       //agregamos algunos contactos
+        ldc.agregarContacto("Sergio", "Perez", "Calle 1", "fernando@gmail.com", "6672034745","6677175551");
+        ldc.agregarContacto("Maria", "Barbara", "Calle 2", "barbara@gmail.com", "667567567","6677175552");
+        ldc.agregarContacto("Alonso", "Cibrian", "Calle 3", "cibrian@gmail.com", "667123456","6671234567");
+        ldc.agregarContacto("Alondra", "Olea", "Calle 4", "olea@gmail.com", "667221707","6677472217");
+        System.out.println("LISTADO DE CONTACTOS");
+        Lista<Contacto> listado = ldc.mostrarTodosLosContactos();
+        for(int i=0; i<listado.getTamanio(); i++){
+            System.out.println(listado.getValor(i));
+        }
+        if(ldc.eliminarContacto("Maria", "Barbara")){
+            System.out.println("Contacto eliminado");
+        System.out.println("LISTADO DE CONTACTOS despues de eliminar ");
+        listado = ldc.mostrarTodosLosContactos();
+        for(int i=0; i<listado.getTamanio(); i++){
+            System.out.println(listado.getValor(i));
+        }
+}
+ldc.modificarContacto("Alonso", "Cibrian", "Calle 5", "cibrian@gmail.com","6678252323","6674626226");
+System.out.println("LISTADO DE CONTACTOS despues de modificar ");
+listado = ldc.mostrarTodosLosContactos();
+for(int i=0; i<listado.getTamanio(); i++){
+    System.out.println(listado.getValor(i));  
+  }
+}
 }
--- EjercicioContactos/ListaDeContactos.java
+++ EjercicioContactos/ListaDeContactos.java
@@ -16,10 +16,35 @@ public class ListaDeContactos {
 	public boolean agregarContacto(String nombres, String apellidos, String direccion,
 	 String correo, String telefono, String celular) throws PosicionIlegalException{
 	 Contacto con = buscarContacto(nombres, apellidos);
-	
-		
-
+	if (con == null){
+		Contacto nuevo = new Contacto(nombres, apellidos, direccion, correo, telefono, celular);
+		contactos.agregar(nuevo);
+		return true;
+	}else{
+		return false;
 	}
+	}
+	//elimina un contacto de la lista si se encuentra
+	//dado sus nombres y apellidos si el usuario existe en la lista
+	//se elimina, return true, en caso contrario return false
+	public boolean eliminarContacto(String nombres, String apellidos) throws PosicionIlegalException{
+		Contacto con = buscarContacto(nombres, apellidos);
+		if (con != null) 
+		{
+		for(int i=0; i<contactos.getTamanio(); i++){
+			Contacto aux = contactos.getValor(i);
+			if(aux.getNombres().equals(nombres) && aux.getApellidos().equals(apellidos)){
+				contactos.remover(i);
+			}
+		}
+		return true;
+		}
+		else
+		{
+		return false;
+		}
+	}
+
 	//busca un contacto en la lista con sus nombres y apellidos
 	//return el contacto si se encuentra y null en caso contrario
 	public Contacto buscarContacto(String nombres, String apellidos) throws PosicionIlegalException{
@@ -29,7 +54,25 @@ public class ListaDeContactos {
 			return con;
 		}
 	}
-	return null;
+	return null; //no se encontro el contacto
+	}
+	//modificar los contactos
+	public boolean modificarContacto(String nombres, String apellidos , String direccion, String correo, String telefono, String celular) throws PosicionIlegalException
+	{
+		//verificar si el contacto existe
+		Contacto con = buscarContacto(nombres, apellidos);
+		if(con == null){
+			return false;
+		}else{
+			con.setDireccion(direccion);
+			con.setCorreo(correo);
+			con.setTelefono(telefono);
+			con.setCelular(celular);
+			return true;
+		}
+	}
 }
 
-}
+
+
+
